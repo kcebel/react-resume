08ca65a26c673c90f1cda4a38a696c9d1874dc06
kcebel <kacy@wecancodeit.org>
2020-05-08 12:14:48 -0400

    Add data.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.scss';
 import {Home} from "./home/Home";
 
 function App() {
+
+  const [resumeData, setState] = useState({})
+
+  useEffect( () => {
+    fetch(`${process.env.PUBLIC_URL}/data.json`)
+      .then(response => response.json())
+      .then(setState)
+      .catch(console.error)
+  }, [])
+
+  if (resumeData.home) {
+    return (
+      <Home name={resumeData.home.name}/>
+    );
+  }
+
   return (
-    <Home name="Kacy"/>
+    <div className="text-centered p-5">
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
   );
+
 }
 
 export default App;
