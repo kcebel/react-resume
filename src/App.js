b758a912fab5f4ec800f68e1b56233b3505bba1c
kcebel <kacy@wecancodeit.org>
2020-05-07 20:00:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Home} from "./home/Home";
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ src/home/Home.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>I'm Kacy</h1>
+    </div>
+  );
+};
